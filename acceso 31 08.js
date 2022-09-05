32f5b5b7b1c212a25e3e974548a81aff82701cc0
AlfonsoGR77 <alfonsogr4554@gmail.com>
2022-09-05 15:10:23 -0500

    todos los apuntes

---
+++ acceso 31 08.js
@@ -0,0 +1,26 @@
+//especificadores de acceso
+// class Persona{
+//     #nombre // # es especificacion de dato 
+//     constructor(nombre){
+//         this.#nombre=nombre;
+//     }
+//     get nombre(){
+//         return this.#nombre
+//     }
+// }
+// let objeto=new Persona ('Luis');
+// console.log(objeto.nombre);
+
+//---------------------------------------------------------------
+
+//agregacion 
+
+
+
+
+
+
+
+
+
+
